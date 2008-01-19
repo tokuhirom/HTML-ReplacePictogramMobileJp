1566812a9ff0b5f3f73c87fa2d1382185188b856
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-01-19 13:19:01 +0000

    lang/perl/HTML-FilterPictogramMobileJp: Checking in changes prior to tagging of version 0.02.  Changelog diff is:
    === Changes
    ==================================================================
    --- Changes	(revision 5225)
    +++ Changes	(local)
    @@ -1,4 +1,11 @@
     Revision history for Perl extension HTML::ReplacePictogramMobileJp
     
    +0.02
    +
    +    - au phone can display docomo sjis hex cref
    +    - au phone can display kddi unicode hex cref
    +    - softbank phone can display unicode hex cref
    +    - oops. return value is bytes instead of unicode string.
    +
     0.01    Fri Jan 18 20:39:18 2008
             - original version
    Unknown target: CHANGES.
    
    
    git-svn-id: http://svn.coderepos.org/share/lang/perl/HTML-ReplacePictogramMobileJp/trunk@4968 d0d07461-0603-4401-acd4-de1884942a52

--- lib/HTML/ReplacePictogramMobileJp.pm
+++ lib/HTML/ReplacePictogramMobileJp.pm
@@ -1,7 +1,7 @@
 package HTML::ReplacePictogramMobileJp;
 use strict;
 use warnings;
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 use Params::Validate ':all';
 use HTML::ReplacePictogramMobileJp::DoCoMo;
 use HTML::ReplacePictogramMobileJp::EZweb;
