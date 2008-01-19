33082c296b557fc989d42be0ca98e9042e62770a
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-01-19 16:12:39 +0000

    lang/perl/HTML-FilterPictogramMobileJp: Checking in changes prior to tagging of version 0.03.  Changelog diff is:
    === Changes
    ==================================================================
    --- Changes	(revision 5258)
    +++ Changes	(local)
    @@ -1,5 +1,10 @@
     Revision history for Perl extension HTML::ReplacePictogramMobileJp
     
    +0.03
    +
    +    - au phone can display <img localsrc="1" />
    +    - oops. added PREREQS.
    +
     0.02
     
         - au phone can display docomo sjis hex cref
    Unknown target: CHANGES.
    
    
    git-svn-id: http://svn.coderepos.org/share/lang/perl/HTML-ReplacePictogramMobileJp/trunk@4999 d0d07461-0603-4401-acd4-de1884942a52

--- lib/HTML/ReplacePictogramMobileJp.pm
+++ lib/HTML/ReplacePictogramMobileJp.pm
@@ -1,7 +1,7 @@
 package HTML::ReplacePictogramMobileJp;
 use strict;
 use warnings;
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 use Params::Validate ':all';
 use HTML::ReplacePictogramMobileJp::DoCoMo;
 use HTML::ReplacePictogramMobileJp::EZweb;
